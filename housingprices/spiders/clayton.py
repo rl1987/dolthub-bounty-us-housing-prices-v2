4ba9be9fe8f327a32cd08933ac99177ab60791f9
rl1987 <rl1987@users.noreply.github.com>
2022-08-28 13:27:04 +0700

    Fetch the form page in ClaytonSpider

--- housingprices/spiders/clayton.py
+++ housingprices/spiders/clayton.py
@@ -4,7 +4,10 @@ import scrapy
 class ClaytonSpider(scrapy.Spider):
     name = 'clayton'
     allowed_domains = ['publicaccess.claytoncountyga.gov']
-    start_urls = ['http://publicaccess.claytoncountyga.gov/']
+    start_urls = ['https://publicaccess.claytoncountyga.gov/search/advancedsearch.aspx?mode=sales']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_form_page)
+
+    def parse_form_page(self, response):
         pass
