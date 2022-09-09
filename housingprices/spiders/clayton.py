68c724f2b51d8ae9235cb1cc4a0cd64deb446be8
rl1987 <rl1987@users.noreply.github.com>
2022-09-09 14:11:33 +0700

    Reproduce POST request that happens when Agree button is clicked

--- housingprices/spiders/clayton.py
+++ housingprices/spiders/clayton.py
@@ -1,13 +1,17 @@
 import scrapy
 
+from scrapy.http import FormRequest
 
 class ClaytonSpider(scrapy.Spider):
     name = 'clayton'
     allowed_domains = ['publicaccess.claytoncountyga.gov']
-    start_urls = ['https://publicaccess.claytoncountyga.gov/search/advancedsearch.aspx?mode=sales']
+    start_urls = ['https://publicaccess.claytoncountyga.gov/Search/Disclaimer.aspx?FromUrl=../search/advancedsearch.aspx?mode=sales']
 
     def start_requests(self):
-        yield scrapy.Request(self.start_urls[0], callback=self.parse_form_page)
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_disclaimer_page)
+
+    def parse_disclaimer_page(self, response):
+        yield FormRequest.from_response(response, formxpath='//form[@name="Form1"]', clickdata={'name':'btAgree'}, callback=self.parse_form_page)
 
     def parse_form_page(self, response):
         pass
