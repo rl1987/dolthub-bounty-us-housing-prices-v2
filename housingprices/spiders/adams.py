c06bf7bb07e2b9d9aeac0000c0377ed205d2dbfe
rl1987 <rl1987@users.noreply.github.com>
2022-09-14 18:04:05 +0700

    Traverse the pages on Adams county site

--- housingprices/spiders/adams.py
+++ housingprices/spiders/adams.py
@@ -4,7 +4,18 @@ import scrapy
 class AdamsSpider(scrapy.Spider):
     name = 'adams'
     allowed_domains = ['gisapp.adcogov.org']
-    start_urls = ['http://gisapp.adcogov.org/']
+    start_urls = ['https://gisapp.adcogov.org/PropertySearch/?selectedAccountTypes=Residential']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_list_page)
+
+    def parse_list_page(self, response):
+        for url in response.xpath('//table//td/a/@href').getall():
+            yield scrapy.Request(url, callback=self.parse_details_page)
+
+        next_page_link = response.xpath('//li[@class="PagedList-skipToNext"]/a/@href').get()
+        if next_page_link is not None:
+            yield response.follow(next_page_link, callback=self.parse_list_page)
+
+    def parse_details_page(self, response):
         pass
