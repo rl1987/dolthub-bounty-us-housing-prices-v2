c2deb9f10c1447f8194721b05e6d58fc00e6f5f4
rl1987 <rl1987@users.noreply.github.com>
2022-09-18 11:30:11 +0700

    Start new spider for Beacon portal

---
+++ housingprices/spiders/beacon.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BeaconSpider(scrapy.Spider):
+    name = 'beacon'
+    allowed_domains = ['beacon.schneidercorp.com']
+    start_urls = ['http://beacon.schneidercorp.com/']
+
+    def parse(self, response):
+        pass
